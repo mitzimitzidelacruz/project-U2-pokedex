dec255691b5e4d9cef110c97d1aa8908d6d12109
vero <veronica.delacruz12@hotmail.com>
2022-07-04 14:21:19 -0500

    Feat: Add new navbar

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css"
+
+const Dir = () => {
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><button><a href="/pokemons"> POKEMONS </a></button></li>
+                    <li><button><a href="/matricula"> ABOUT </a></button></li>
+                </ul>
+            </nav>
+        </div>
+    );
+
+};
+
+export default Dir;
