1afffb5ad2289f289e979e93c4306245ebfd711d
vero <veronica.delacruz12@hotmail.com>
2022-07-04 14:50:29 -0500

    Style: Add new changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './home';
+import App from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
