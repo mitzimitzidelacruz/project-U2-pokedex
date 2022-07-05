c6024ad846bf1006d2c6f7543dea9aab4a64e9ff
vero <veronica.delacruz12@hotmail.com>
2022-07-04 19:22:20 -0500

    Feat: Add new changes to file index

--- src/about.js
+++ src/about.js
@@ -5,36 +5,41 @@ const Me = () => {
 
     return (
         <div id="aboutme">
-            <div >
+            <div>
                 <img id="cara" src="./img/cara.jpg"></img>
                 <img id="yo" src="./img/mitzi.jpeg " alt="yo" ></img>
                 <img id="cara" src="./img/descarga (1).jpg"></img>
-            </div>
-            <div>
+          
+           
             <h3>MITZI VERÓNICA ESPARZA DE LA CRUZ</h3><br></br>
             <h5>ESTOS SON MIS HOBBIES</h5><br></br>
-
+  
+         
+    
+            <div id="hobbies">
             <tr>
-                <td>1-Practic</td>
+                <td  id="hobbies">1-Practicar boxeo</td>
             </tr>
             <tr>
-                <td>2-Escuchar música </td>
+                <td  id="hobbies">2-Escuchar música </td>
             </tr>
             <tr>
-                <td>3-Obsesionarme con series y/o peliculas</td>
+                <td  id="hobbies">3-Obsesionarme con series y/o peliculas</td>
             </tr>
             <tr>
-                <td>4-Salir de viaje siempre que puedo</td>
+                <td  id="hobbies">4-Salir de viaje siempre que puedo</td>
             </tr>
             <tr>
-                <td>5-Salir de fiesta siempre que tengo la oportunidad</td>
+                <td  id="hobbies">5-Salir de fiesta siempre que tengo la oportunidad</td>
             </tr>
-        <table>
-          
-        </table>
+            <h3>Tengo 21 años de edad</h3>
+            <h3>Soy Acuario</h3>
+            <h3>Me gusta la escuela</h3>
     </div>
-            </div>
-     
+    </div>
+    </div>
+   
+      
      );
 
 };
